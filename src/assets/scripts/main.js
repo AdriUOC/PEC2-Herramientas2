c42b8cf8b0b81a704ce24caac0e05cd24bec13bf
Adrian Sanchez Cabrera <adrian@uoc.edu>
2023-12-02 02:18:18 +0100

    Casi finiquitada la PEC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -15,3 +15,14 @@ import * as bootstrap from 'bootstrap';
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+ // JavaScript para manejar el evento de clic en el botón
+ document.getElementById('mostrarToast').addEventListener('click', function() {
+  // Crear un nuevo toast
+  var toast = new bootstrap.Toast(document.getElementById('miToast'));
+  // Mostrar el toast
+  toast.show();
+});
